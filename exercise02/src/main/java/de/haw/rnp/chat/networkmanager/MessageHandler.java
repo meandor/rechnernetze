5ba14e43bde35e48ceab6912045d8f6ff506c58b
Daniel Schruhl <danielschruhl@gmail.com>
2016-05-28 18:11:10 +0200

    MessageHandler interface added

---
+++ exercise02/src/main/java/de/haw/rnp/chat/networkmanager/MessageHandler.java
@@ -0,0 +1,29 @@
+package de.haw.rnp.chat.networkmanager;
+
+import de.haw.rnp.chat.model.Message;
+import de.haw.rnp.chat.model.User;
+
+import java.net.InetAddress;
+import java.util.List;
+
+/**
+ * Interface for handling protocol messages.
+ */
+public interface MessageHandler {
+
+    void processMessage(String protocolMessage);
+
+    User login(String name, InetAddress hostName, int port);
+
+    void logout(User user);
+
+    void sendMessage(Message message);
+
+    void receiveMessage(Message message);
+
+    void sendName(User user, List recipient);
+
+    void changeName(String name, InetAddress hostName, int port);
+
+    User initialConnect(InetAddress hostName, int port);
+}
