43c7b78e53c2abaf7f86d98315d5eda20f50770a
Daniel Schruhl <danielschruhl@gmail.com>
2016-05-29 14:28:03 +0200

    each user has also now port and hostname

--- exercise02/src/main/java/de/haw/rnp/chat/model/User.java
+++ exercise02/src/main/java/de/haw/rnp/chat/model/User.java
@@ -2,6 +2,7 @@ package de.haw.rnp.chat.model;
 
 import de.haw.rnp.chat.networkmanager.Node;
 
+import java.net.InetAddress;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -12,15 +13,19 @@ public class User {
     private String name;
     private Node clientNode;
     private Node serverNode;
+    private InetAddress hostName;
+    private int port;
 
     /**
      * @param name       the name of the user
      * @param clientNode the clientNode attached to a user
      */
 
-    public User(String name, Node clientNode) {
+    public User(String name, Node clientNode, InetAddress hostName, int port) {
         this.setName(name);
         this.setClientNode(clientNode);
+        this.port = port;
+        this.hostName = hostName;
     }
 
 
@@ -46,6 +51,22 @@ public class User {
 
     public void setServerNode(Node serverNode) {
         this.serverNode = serverNode;
+    }
+
+    public InetAddress getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(InetAddress hostName) {
+        this.hostName = hostName;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
     }
 
     @Override
