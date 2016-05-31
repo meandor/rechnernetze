fa7623b6786eb10c97c73cbdbf7db38a967fa4e2
Daniel Schruhl <danielschruhl@gmail.com>
2016-05-31 14:06:33 +0200

    Refactoring

--- exercise02/src/main/java/de/haw/rnp/chat/Main.java
+++ exercise02/src/main/java/de/haw/rnp/chat/Main.java
@@ -1,22 +1,14 @@
 package de.haw.rnp.chat;
 
 import de.haw.rnp.chat.controller.Controller;
-import de.haw.rnp.chat.networkmanager.MessageHandler;
-import de.haw.rnp.chat.networkmanager.Node;
-import de.haw.rnp.chat.networkmanager.tasks.ServerAwaitConnectionsTask;
-import de.haw.rnp.chat.networkmanager.tasks.ServerStartTask;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-import java.net.InetAddress;
-import java.util.concurrent.Future;
-import java.util.concurrent.ThreadPoolExecutor;
-
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Controller controller = new Controller(primaryStage);
-        MessageHandler messageHandler = controller.getMessageHandler();
+        /*MessageHandler messageHandler = controller.getMessageHandler();
         ThreadPoolExecutor executor = ((ThreadPoolExecutor) messageHandler.getExecutor());
         Node server = messageHandler.getFactory().createNode(InetAddress.getByName("0.0.0.0"), 18181);
         ServerStartTask task = new ServerStartTask(server);
@@ -24,6 +16,6 @@ public class Main extends Application {
         if (serverStarted.get()) {
             ServerAwaitConnectionsTask task2 = new ServerAwaitConnectionsTask(server);
             executor.execute(task2);
-        }
+        }*/
     }
 }
--- exercise02/src/main/java/de/haw/rnp/chat/controller/Controller.java
+++ exercise02/src/main/java/de/haw/rnp/chat/controller/Controller.java
@@ -2,31 +2,30 @@ package de.haw.rnp.chat.controller;
 
 import de.haw.rnp.chat.model.Message;
 import de.haw.rnp.chat.model.User;
-import de.haw.rnp.chat.networkmanager.ChatProtocolMessageHandler;
-import de.haw.rnp.chat.networkmanager.MessageHandler;
-import de.haw.rnp.chat.networkmanager.Node;
+import de.haw.rnp.chat.networkmanager.OutgoingChatProtocolMessageHandler;
+import de.haw.rnp.chat.networkmanager.OutgoingMessageHandler;
 import de.haw.rnp.chat.view.IView;
 import de.haw.rnp.chat.view.ViewController;
-import javafx.application.Application;
 import javafx.stage.Stage;
+
 import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class Controller implements IControllerService{
+public class Controller implements IControllerService {
     private BlockingQueue messageQueue;
     private User loggedInUser;
     private List<User> userList;
     private IView view;
-    private MessageHandler messageHandler;
+    private OutgoingMessageHandler outgoingMessageHandler;
 
     public Controller(Stage stage) {
         this.messageQueue = new LinkedBlockingQueue();
         this.userList = new ArrayList<>();
         this.view = new ViewController(stage, this);
-        this.messageHandler = new ChatProtocolMessageHandler(this);
+        this.outgoingMessageHandler = new OutgoingChatProtocolMessageHandler(this);
     }
 
     public BlockingQueue getMessageQueue() {
@@ -51,37 +50,36 @@ public class Controller implements IControllerService{
     }
 
     @Override
-    public String login(String userName, InetAddress address, InetAddress localAddress, int  port, int localport) {
-        Node node = messageHandler.initialConnect(address, port);
-        //User user = messageHandler.login(node,userName,port);
-        /*if(!user.equals(null))
-            loggedInUser = user;
-        return loggedInUser.getName();*/
-        return "asd";
+    public String login(String userName, InetAddress address, InetAddress localAddress, int port, int localport) {
+        this.loggedInUser = this.outgoingMessageHandler.login(address, port, userName, localAddress, localport);
+        if (this.loggedInUser == null) {
+            return "";
+        }
+        return userName;
     }
 
     @Override
     public void logout() {
-        messageHandler.logout(loggedInUser, userList.get(0));
+        InetAddress activePeerHostName = userList.get(0).getHostName();
+        int activePeerPort = userList.get(0).getPort();
+        outgoingMessageHandler.logout(activePeerHostName, activePeerPort, loggedInUser.getHostName(), loggedInUser.getPort());
     }
 
     @Override
     public boolean sendMessage(String recipient, String message) {
         User user = getUserByName(recipient);
-        if(user.equals(null))
+        if (user.equals(null))
             return false;
-        Message ms = new Message(message, loggedInUser, user);
-        messageHandler.sendMessage(ms);
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(user);
+        Message ms = new Message(message, loggedInUser, userList);
+        outgoingMessageHandler.sendMessage(ms);
         return true;
     }
 
-    public MessageHandler getMessageHandler() {
-        return messageHandler;
-    }
-
-    private User getUserByName(String userName){
-        for(User user : userList){
-            if(user.getName().equals(userName))
+    private User getUserByName(String userName) {
+        for (User user : userList) {
+            if (user.getName().equals(userName))
                 return user;
         }
         return null;
