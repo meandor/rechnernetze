664c75055256149eb3b93ff700979fdfd39a2882
Daniel Schruhl <danielschruhl@gmail.com>
2016-05-31 18:27:51 +0200

    small refactor

--- exercise02/src/main/java/de/haw/rnp/chat/view/IView.java
+++ exercise02/src/main/java/de/haw/rnp/chat/view/IView.java
@@ -1,12 +1,11 @@
 package de.haw.rnp.chat.view;
 
-import de.haw.rnp.chat.model.Message;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public interface IView {
-    public void setUserLoggedIn(String userName);
-    public void updateUserlist(List<String> usernames);
-    public void appendMessage(String from, String message);
+    void setUserLoggedIn(String userName);
+
+    void updateUserlist(List<String> usernames);
+
+    void appendMessage(String from, String message);
 }
