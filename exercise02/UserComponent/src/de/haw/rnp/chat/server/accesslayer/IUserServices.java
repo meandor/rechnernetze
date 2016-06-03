fdd0402ce07bfe45290ca3f98641e0b7d2473615
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-04 01:04:24 +0200

    added general user interface

--- exercise02/UserComponent/src/de/haw/rnp/chat/server/accesslayer/IUserServices.java
+++ exercise02/UserComponent/src/de/haw/rnp/chat/server/accesslayer/IUserServices.java
@@ -1,4 +1,27 @@
 package de.haw.rnp.chat.server.accesslayer;
 
-public class IUserServices {
+import de.haw.rnp.chat.server.dataaccesslayer.entities.*;
+
+import java.net.InetAddress;
+
+public interface IUserServices {
+
+    public void createLocalUser(User user);
+
+    public User getLocalUser();
+
+    public void createUser(User user);
+
+    public Userlist getAllActiveUsers();
+
+    public User removeUserFromActiveUsers(User user);
+
+    public User findUserByAddress(InetAddress address);
+
+    public User findUserByUsername(String username);
+
+    public void updateUser(User user);
+
+    public void updateLocalUser(User user);
+
 }
