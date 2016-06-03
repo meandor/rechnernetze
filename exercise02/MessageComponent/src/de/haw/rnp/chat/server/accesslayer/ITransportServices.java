c22b64faaefde60ccd8ac2899e902afbc08ca935
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-04 01:04:10 +0200

    added general transport interface

--- exercise02/MessageComponent/src/de/haw/rnp/chat/server/accesslayer/ITransportServices.java
+++ exercise02/MessageComponent/src/de/haw/rnp/chat/server/accesslayer/ITransportServices.java
@@ -1,4 +1,15 @@
 package de.haw.rnp.chat.server.accesslayer;
 
+import de.haw.rnp.chat.server.dataaccesslayer.entities.Frame;
+
 public interface ITransportServices {
+
+    public void sendMessage(Frame frame);
+
+    public void sendLogin(Frame frame);
+
+    public void sendLogout(Frame frame);
+
+    public void SendUsername(Frame frame);
+
 }
