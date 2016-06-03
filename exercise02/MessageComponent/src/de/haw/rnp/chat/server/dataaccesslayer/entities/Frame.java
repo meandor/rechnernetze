d2515aacfa6a4ca9a4b7367dc028e58aebd8c446
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-03 20:55:40 +0200

    added toBytes method for frame.

--- exercise02/MessageComponent/src/de/haw/rnp/chat/server/dataaccesslayer/entities/Frame.java
+++ exercise02/MessageComponent/src/de/haw/rnp/chat/server/dataaccesslayer/entities/Frame.java
@@ -1,19 +1,24 @@
 package de.haw.rnp.chat.server.dataaccesslayer.entities;
 
 import de.haw.rnp.chat.server.dataaccesslayer.enumerations.MessageType;
+import de.haw.rnp.chat.util.ChatUtil;
 
 import java.util.Collection;
 
 public class Frame {
+    private User sender;
+    private User recipient;
     private int version;
     private MessageType messageType;
     private int length;
     private Collection<Field> fields;
 
-    public Frame(int version, MessageType message, int length){
+    public Frame(User sender, User recipient, int version, MessageType messageType, int length){
         this.version  = version;
         this.messageType = messageType;
         this.length = length;
+        this.sender = sender;
+        this.recipient = recipient;
     }
 
     public void addField(Field field){
@@ -50,5 +55,26 @@ public class Frame {
 
     public void setFields(Collection<Field> fields) {
         this.fields = fields;
+    }
+
+    public byte[] getFrameAsBytes(){
+        byte[] result = new byte[0];
+        byte[] reserved = new byte[]{0x0, 0x0};
+
+        //building the header
+        ChatUtil.concat(result,
+                        ChatUtil.intToOneByteArray(version),
+                        messageType.getCodeAsArray(),
+                        reserved,
+                        sender.getAddressAsBytes(),
+                        sender.getPortAsBytes(),
+                        ChatUtil.intToByteArray(length));
+
+        //adding fields
+        for(Field field : fields){
+            ChatUtil.concat(result, field.getFieldAsBytes());
+        }
+
+        return result;
     }
 }
