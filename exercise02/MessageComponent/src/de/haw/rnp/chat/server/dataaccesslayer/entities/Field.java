aa8044c3077ce7b7f9c7cdb8e599932951754340
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-03 17:54:59 +0200

    added field entity

--- exercise02/MessageComponent/src/de/haw/rnp/chat/server/dataaccesslayer/entities/Field.java
+++ exercise02/MessageComponent/src/de/haw/rnp/chat/server/dataaccesslayer/entities/Field.java
@@ -1,4 +1,39 @@
 package de.haw.rnp.chat.server.dataaccesslayer.entities;
 
-public class Field {
+import de.haw.rnp.chat.server.dataaccesslayer.enumerations.FieldType;
+
+public class Field<T> {
+    private FieldType fieldType;
+    private int length;
+    private T data;
+
+    public Field(FieldType fieldType, int length, T data){
+        this.fieldType = fieldType;
+        this.length = length;
+        this.data = data;
+    }
+
+    public FieldType getFieldType() {
+        return fieldType;
+    }
+
+    public void setFieldType(FieldType fieldType) {
+        this.fieldType = fieldType;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
