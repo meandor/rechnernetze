c364ae8a6c8a9a3038ba373bd7fc9b497e205f25
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-05 20:13:11 +0200

    added method to interface

--- exercise02/OutgoingPeerAdapter/src/de/haw/rnp/chat/server/accesslayer/IOutgoingPeerAdapterServices.java
+++ exercise02/OutgoingPeerAdapter/src/de/haw/rnp/chat/server/accesslayer/IOutgoingPeerAdapterServices.java
@@ -1,4 +1,7 @@
 package de.haw.rnp.chat.server.accesslayer;
 
+import de.haw.rnp.chat.util.AddressType;
+
 public interface IOutgoingPeerAdapterServices {
+    public boolean sendData(AddressType address, byte[] data);
 }
