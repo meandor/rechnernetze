3099a46a13b3eae3962e5ed90e0af20964dd45c9
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-05 21:49:19 +0200

    implemented sendData method with a single socket connect.

--- exercise02/OutgoingPeerAdapter/src/de/haw/rnp/chat/server/businesslogiclayer/OutgoingPeerAdapterBusinessLogic.java
+++ exercise02/OutgoingPeerAdapter/src/de/haw/rnp/chat/server/businesslogiclayer/OutgoingPeerAdapterBusinessLogic.java
@@ -3,10 +3,22 @@ package de.haw.rnp.chat.server.businesslogiclayer;
 import de.haw.rnp.chat.server.accesslayer.IOutgoingPeerAdapterServices;
 import de.haw.rnp.chat.util.AddressType;
 
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class OutgoingPeerAdapterBusinessLogic implements IOutgoingPeerAdapterServices{
 
     @Override
     public boolean sendData(AddressType address, byte[] data) {
-        return false;
+        try {
+            Socket socket = new Socket(address.getIp(), address.getPort());
+            DataOutputStream outputStream = new 
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 }
