fb0646ead75f11ff8b476d18d10557e9a833e1f8
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-05 20:13:44 +0200

    implemented interface to business logic

--- exercise02/OutgoingPeerAdapter/src/de/haw/rnp/chat/server/businesslogiclayer/OutgoingPeerAdapterBusinessLogic.java
+++ exercise02/OutgoingPeerAdapter/src/de/haw/rnp/chat/server/businesslogiclayer/OutgoingPeerAdapterBusinessLogic.java
@@ -1,6 +1,12 @@
 package de.haw.rnp.chat.server.businesslogiclayer;
 
 import de.haw.rnp.chat.server.accesslayer.IOutgoingPeerAdapterServices;
+import de.haw.rnp.chat.util.AddressType;
 
 public class OutgoingPeerAdapterBusinessLogic implements IOutgoingPeerAdapterServices{
+
+    @Override
+    public boolean sendData(AddressType address, byte[] data) {
+        return false;
+    }
 }
