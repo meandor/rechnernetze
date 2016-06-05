e7c9fc26d741ec61bb3cc9bf76c2ba8b55934691
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-05 19:57:54 +0200

    added outgoing peer adapter layout

---
+++ exercise02/OutgoingPeerAdapter/src/de/haw/rnp/chat/server/businesslogiclayer/OutgoingPeerAdapterBusinessLogic.java
@@ -0,0 +1,6 @@
+package de.haw.rnp.chat.server.businesslogiclayer;
+
+import de.haw.rnp.chat.server.accesslayer.IOutgoingPeerAdapterServices;
+
+public class OutgoingPeerAdapterBusinessLogic implements IOutgoingPeerAdapterServices{
+}
