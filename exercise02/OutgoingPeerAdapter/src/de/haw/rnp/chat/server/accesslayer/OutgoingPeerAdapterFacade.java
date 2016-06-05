ee91518aa76556cc6903871cc4aa7dcb02f63796
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-05 20:14:08 +0200

    implemented interface to facade

--- exercise02/OutgoingPeerAdapter/src/de/haw/rnp/chat/server/accesslayer/OutgoingPeerAdapterFacade.java
+++ exercise02/OutgoingPeerAdapter/src/de/haw/rnp/chat/server/accesslayer/OutgoingPeerAdapterFacade.java
@@ -1,4 +1,18 @@
 package de.haw.rnp.chat.server.accesslayer;
 
+import de.haw.rnp.chat.server.businesslogiclayer.OutgoingPeerAdapterBusinessLogic;
+import de.haw.rnp.chat.util.AddressType;
+
 public class OutgoingPeerAdapterFacade implements IOutgoingPeerAdapterServices{
+
+    private OutgoingPeerAdapterBusinessLogic businessLogic;
+
+    public OutgoingPeerAdapterFacade(){
+        businessLogic = new OutgoingPeerAdapterBusinessLogic();
+    }
+
+    @Override
+    public boolean sendData(AddressType address, byte[] data) {
+        return businessLogic.sendData(address, data);
+    }
 }
