8e9f0920706cfa17a7efeebbcdd835c670340c2c
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-05 19:58:16 +0200

    added address type to util

---
+++ exercise02/Utilities/src/de/haw/rnp/chat/util/AddressType.java
@@ -0,0 +1,24 @@
+package de.haw.rnp.chat.util;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class AddressType {
+
+    private InetAddress ip;
+    private int port;
+
+    public AddressType(InetAddress ip, int port){
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public AddressType(String ip, int port){
+        try {
+            this.ip = InetAddress.getByName(ip);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        this.port = port;
+    }
+}
