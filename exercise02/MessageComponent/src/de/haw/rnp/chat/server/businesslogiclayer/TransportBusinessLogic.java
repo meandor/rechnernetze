4fa2353233f0e36e2f817a4bb5ffc209a26975fb
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-05 08:07:58 +0200

    implemented interface to BL

--- exercise02/MessageComponent/src/de/haw/rnp/chat/server/businesslogiclayer/TransportBusinessLogic.java
+++ exercise02/MessageComponent/src/de/haw/rnp/chat/server/businesslogiclayer/TransportBusinessLogic.java
@@ -1,4 +1,34 @@
 package de.haw.rnp.chat.server.businesslogiclayer;
 
-public class TransportBusinessLogic {
+import de.haw.rnp.chat.server.accesslayer.ITransportServices;
+import de.haw.rnp.chat.server.accesslayer.IUserServices;
+import de.haw.rnp.chat.server.dataaccesslayer.entities.Frame;
+
+public class TransportBusinessLogic implements ITransportServices {
+
+    private IUserServices userServices;
+
+    public TransportBusinessLogic(IUserServices userServices){
+        this.userServices = userServices;
+    }
+
+    @Override
+    public void sendMessage(Frame frame) {
+
+    }
+
+    @Override
+    public void sendLogin(Frame frame) {
+
+    }
+
+    @Override
+    public void sendLogout(Frame frame) {
+
+    }
+
+    @Override
+    public void SendUsername(Frame frame) {
+
+    }
 }
