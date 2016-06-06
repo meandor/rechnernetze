796a537c88ba251b778211154d96d0951f477d5c
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-07 00:02:51 +0200

    added socket worker to read from socket and enqueue data

--- exercise02/IncomingPeerAdapter/src/de/haw/rnp/chat/server/dataaccesslayer/SocketWorker.java
+++ exercise02/IncomingPeerAdapter/src/de/haw/rnp/chat/server/dataaccesslayer/SocketWorker.java
@@ -1,4 +1,33 @@
 package de.haw.rnp.chat.server.dataaccesslayer;
 
-public class SocketWorker {
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.concurrent.BlockingQueue;
+
+import static org.apache.commons.io.IOUtils.toByteArray;
+
+public class SocketWorker implements Runnable{
+
+    private Socket clientSocket;
+    private BlockingQueue<byte[]> queue;
+
+    public SocketWorker(Socket clientSocket, BlockingQueue<byte[]> queue) {
+        this.clientSocket = clientSocket;
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            InputStream input  = clientSocket.getInputStream();
+            byte[] tmp = IOUtils.toByteArray(input);
+            input.close();
+            queue.offer(tmp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
