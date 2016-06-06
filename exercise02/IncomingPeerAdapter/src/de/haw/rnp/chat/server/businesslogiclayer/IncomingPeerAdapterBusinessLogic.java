0d97be4cda278e756873ba1e192863aa551c6b27
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-07 00:03:25 +0200

    implemented BL

--- exercise02/IncomingPeerAdapter/src/de/haw/rnp/chat/server/businesslogiclayer/IncomingPeerAdapterBusinessLogic.java
+++ exercise02/IncomingPeerAdapter/src/de/haw/rnp/chat/server/businesslogiclayer/IncomingPeerAdapterBusinessLogic.java
@@ -2,15 +2,20 @@ package de.haw.rnp.chat.server.businesslogiclayer;
 
 import de.haw.rnp.chat.server.accesslayer.IIncomingPeerAdapterServices;
 import de.haw.rnp.chat.server.accesslayer.ITransportServicesForIncomingPeerAdapter;
+import de.haw.rnp.chat.server.dataaccesslayer.QueueWorker;
+import de.haw.rnp.chat.server.dataaccesslayer.Server;
 import de.haw.rnp.chat.util.AddressType;
 
+import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class IncomingPeerAdapterBusinessLogic implements IIncomingPeerAdapterServices {
 
     private ITransportServicesForIncomingPeerAdapter transportServices;
-    private BlockingQueue queue;
+    private BlockingQueue<byte[]> queue;
+    private Server server;
+    private QueueWorker queueWorker;
 
     public IncomingPeerAdapterBusinessLogic(ITransportServicesForIncomingPeerAdapter transportServices){
         this.transportServices = transportServices;
@@ -19,11 +24,23 @@ public class IncomingPeerAdapterBusinessLogic implements IIncomingPeerAdapterSer
 
     @Override
     public void startServer(AddressType address) {
+        server = new Server(address.getPort(), queue);
+        new Thread(server).start();
+    }
 
+    @Override
+    public void stopServer() {
+        server.stop();
     }
 
     @Override
     public void startQueueWorker() {
+        queueWorker = new QueueWorker(transportServices, queue);
+        new Thread(queueWorker).start();
+    }
 
+    @Override
+    public void stopQueueWorker() {
+        queueWorker.stop();
     }
 }
