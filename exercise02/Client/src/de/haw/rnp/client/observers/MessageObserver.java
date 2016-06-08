8d6f1a36aa21c3ce590b58609a2e0f5ec3b2b3d8
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-08 08:49:42 +0200

    added layout for the observers

--- exercise02/Client/src/de/haw/rnp/client/observers/MessageObserver.java
+++ exercise02/Client/src/de/haw/rnp/client/observers/MessageObserver.java
@@ -1,10 +1,15 @@
 package de.haw.rnp.client.observers;
 
+import de.haw.rnp.adapter.outgoingclient.dataaccesslayer.MessageDTO;
 import de.haw.rnp.util.ChatUtil;
 import de.haw.rnp.util.IObserver;
 
+import java.util.ArrayList;
+
 public class MessageObserver implements IObserver<MessageDTO>{
 
+    @Override
+    public void update(MessageDTO elem) {
 
-
+    }
 }
--- exercise02/Client/src/de/haw/rnp/client/observers/UserlistObserver.java
+++ exercise02/Client/src/de/haw/rnp/client/observers/UserlistObserver.java
@@ -1,4 +1,13 @@
 package de.haw.rnp.client.observers;
 
-public class UserlistObserver {
+import de.haw.rnp.adapter.outgoingclient.dataaccesslayer.UserDTO;
+import de.haw.rnp.util.IObserver;
+
+import java.util.ArrayList;
+
+public class UserlistObserver implements IObserver<ArrayList<UserDTO>> {
+    @Override
+    public void update(ArrayList<UserDTO> elem) {
+
+    }
 }
