05f21d8de5c8fbbdbf109435e26220617df22bf8
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-08 13:46:14 +0200

    implemented methods

--- exercise02/OutgoingClientAdapter/src/de/haw/rnp/adapter/outgoingclient/dataaccesslayer/MessageSubject.java
+++ exercise02/OutgoingClientAdapter/src/de/haw/rnp/adapter/outgoingclient/dataaccesslayer/MessageSubject.java
@@ -7,20 +7,28 @@ import java.util.ArrayList;
 
 public class MessageSubject implements ISubject {
 
-    private ArrayList<IObserver> observers = new ArrayList<IObserver>();
+    private ArrayList<IObserver> observers = new ArrayList<>();
+    private MessageDTO lastMessage;
 
     @Override
     public void registerObserver(IObserver observer) {
-
+        observers.add(observer);
     }
 
     @Override
     public void removeObserver(IObserver observer) {
-
+        observers.remove(observer);
     }
 
     @Override
     public void notifyObservers() {
+        for(IObserver<MessageDTO> observer : observers){
+            observer.update(lastMessage);
+        }
+    }
 
+    public void addMessage(MessageDTO message){
+        lastMessage = message;
+        notifyObservers();
     }
 }
