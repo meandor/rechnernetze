3f2195331ea00cfe4454f54b2b88f9cc7f4da59a
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-08 08:50:50 +0200

    finished user subject

---
+++ exercise02/OutgoingClientAdapter/src/de/haw/rnp/adapter/outgoingclient/dataaccesslayer/UserSubject.java
@@ -0,0 +1,63 @@
+package de.haw.rnp.adapter.outgoingclient.dataaccesslayer;
+
+import de.haw.rnp.util.AddressType;
+import de.haw.rnp.util.IObserver;
+import de.haw.rnp.util.ISubject;
+
+import java.util.ArrayList;
+
+public class UserSubject implements ISubject{
+
+    private ArrayList<IObserver> observers = new ArrayList<>();
+    private ArrayList<UserDTO> users = new ArrayList<>();
+
+    @Override
+    public void registerObserver(IObserver observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(IObserver observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for(IObserver<ArrayList<UserDTO>> observer : observers){
+            observer.update(users);
+        }
+    }
+
+    public ArrayList<UserDTO> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<UserDTO> users) {
+        this.users = users;
+        notifyObservers();
+    }
+
+    public void addUser(UserDTO user){
+        users.add(user);
+        notifyObservers();
+    }
+
+    public void removeUserByAddressType(AddressType address){
+        users.remove(findUserByAddressType(address));
+        notifyObservers();
+    }
+
+    public void updateUsername(UserDTO user){
+        removeUserByAddressType(user.getAddress());
+        addUser(user);
+    }
+
+    private UserDTO findUserByAddressType(AddressType address){
+        for(UserDTO user : users){
+            if(user.getAddress().getIp().equals(address.getIp()) &&
+               user.getAddress().getPort() == address.getPort())
+                return user;
+        }
+        return null;
+    }
+}
