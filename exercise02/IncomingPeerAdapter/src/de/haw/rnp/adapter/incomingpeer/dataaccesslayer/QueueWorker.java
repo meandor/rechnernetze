00ec5bc82be3a3b5cc7c759ce90d3aa2e08a9b4b
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-09 14:34:47 +0200

    added frame handling to queue worker

--- exercise02/IncomingPeerAdapter/src/de/haw/rnp/adapter/incomingpeer/dataaccesslayer/QueueWorker.java
+++ exercise02/IncomingPeerAdapter/src/de/haw/rnp/adapter/incomingpeer/dataaccesslayer/QueueWorker.java
@@ -1,7 +1,11 @@
 package de.haw.rnp.adapter.incomingpeer.dataaccesslayer;
 
+import de.haw.rnp.adapter.outgoingclient.accesslayer.IOutClientAdapterServicesForInPeer;
+import de.haw.rnp.adapter.outgoingclient.dataaccesslayer.MessageDTO;
+import de.haw.rnp.adapter.outgoingclient.dataaccesslayer.UserDTO;
 import de.haw.rnp.component.transport.accesslayer.ITransportServicesForIncomingPeerAdapter;
 import de.haw.rnp.component.transport.dataaccesslayer.entities.Frame;
+import de.haw.rnp.util.enumerations.MessageType;
 
 import java.io.IOException;
 import java.util.concurrent.BlockingQueue;
@@ -10,10 +14,14 @@ public class QueueWorker implements Runnable{
 
     private BlockingQueue<byte[]> queue;
     private ITransportServicesForIncomingPeerAdapter transportServices;
+    private IOutClientAdapterServicesForInPeer outClientAdapterServices;
     private boolean isRunning;
     private Thread runningThread;
 
-    public QueueWorker(ITransportServicesForIncomingPeerAdapter transportServices, BlockingQueue<byte[]> queue) {
+    public QueueWorker(ITransportServicesForIncomingPeerAdapter transportServices,
+                       IOutClientAdapterServicesForInPeer outClientAdapterServices,
+                       BlockingQueue<byte[]> queue) {
+        this.outClientAdapterServices = outClientAdapterServices;
         this.transportServices = transportServices;
         this.queue = queue;
     }
@@ -29,6 +37,30 @@ public class QueueWorker implements Runnable{
             try {
                 byte[] tmp = queue.take();
                 Frame frame = transportServices.receiveFrameAsBytes(tmp);
+
+                switch(frame.getMessageType()){
+                    case Login:{
+                        UserDTO user = frame.toUserDTO();
+                        if(user == null)
+                            break;
+                        outClientAdapterServices.addUser(user);
+                        break;
+                    }
+                    case TextMessage:{
+                        MessageDTO msg = frame.toMessageDTO();
+                        if(msg == null)
+                            break;
+                        outClientAdapterServices.addMessage(msg);
+                        break;
+                    }
+                    case Logout:{
+                        UserDTO user = frame.toUserDTO();
+                        if(user == null)
+                            break;
+                        outClientAdapterServices.removeUser(user);
+                    }
+
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
