bfa3ae1cb710e0af785b0780fb166a7d79cf4942
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-09 11:14:12 +0200

    added interface

--- exercise02/IncomingClientAdapter/src/de/haw/rnp/adapter/incomingclient/accesslayer/IInClientAdapterServices.java
+++ exercise02/IncomingClientAdapter/src/de/haw/rnp/adapter/incomingclient/accesslayer/IInClientAdapterServices.java
@@ -1,4 +1,18 @@
 package de.haw.rnp.adapter.incomingclient.accesslayer;
 
+import de.haw.rnp.adapter.incomingclient.dataaccesslayer.FrameDTO;
+import de.haw.rnp.util.AddressType;
+
 public interface IInClientAdapterServices {
+
+    boolean startServer(AddressType address);
+
+    boolean sendMessage(FrameDTO frame);
+
+    boolean sendLogin(FrameDTO frame);
+
+    boolean sendLogout(FrameDTO frame);
+
+    boolean sendUsername(FrameDTO frame);
+
 }
