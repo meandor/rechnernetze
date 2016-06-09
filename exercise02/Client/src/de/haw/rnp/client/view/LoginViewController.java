cb0042cf87168db77d87ce45cebac8c3996e16d1
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-09 08:56:44 +0200

    implemented new LoginView logic

--- exercise02/Client/src/de/haw/rnp/client/view/LoginViewController.java
+++ exercise02/Client/src/de/haw/rnp/client/view/LoginViewController.java
@@ -5,6 +5,11 @@ import javafx.scene.Scene;
 public class LoginViewController {
 
     private LoginView loginView;
+    private ViewController controller;
+
+    public LoginViewController(ViewController controller){
+        this.controller = controller;
+    }
 
     public Scene initLoginView() {
         loginView = new LoginView();
@@ -14,15 +19,12 @@ public class LoginViewController {
 
     private void initOnEvents(){
         loginView.getSignin().setOnAction(action -> {
-            String user = loginView.getUserTextField().getText() + " - me";
+            String user = loginView.getUserTextField().getText();
             String host = loginView.getHostTextField().getText();
-            String port = loginView.getPortTextField().getText();
-            try {
-                if (controller.login(user, InetAddress.getByName(host), serverHostName, Integer.parseInt(port), serverPort)) {
-                    initChatView(controller.getUserList());
-                }
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
+            int port = Integer.parseInt(loginView.getPortTextField().getText());
+
+            if(controller.sendLogin(user, host, port)){
+                controller.changeViewState(ViewController.ViewState.Chat);
             }
         });
     }
