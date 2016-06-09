7864cbfa9f3768c7505f6f750910771259b86ec5
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-09 09:37:40 +0200

    added FieldDTO

---
+++ exercise02/IncomingClientAdapter/src/de/haw/rnp/adapter/incomingclient/dataaccesslayer/FieldDTO.java
@@ -0,0 +1,16 @@
+package de.haw.rnp.adapter.incomingclient.dataaccesslayer;
+
+import de.haw.rnp.util.enumerations.FieldType;
+
+public class FieldDTO<T> {
+
+    private FieldType fieldType;
+    private int length;
+    private T data;
+
+    public FieldDTO(FieldType fieldType, int length, T data){
+        this.fieldType = fieldType;
+        this.length = length;
+        this.data = data;
+    }
+}
