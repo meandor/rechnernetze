f9e3fb1bae5e21fc6af8075cb2e457524faa944d
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-09 09:37:12 +0200

    added interface methods, not implemented yet

--- exercise02/Client/src/de/haw/rnp/client/Controller.java
+++ exercise02/Client/src/de/haw/rnp/client/Controller.java
@@ -1,13 +1,44 @@
 package de.haw.rnp.client;
 
+import de.haw.rnp.client.model.User;
+import de.haw.rnp.client.observers.MessageObserver;
+import de.haw.rnp.client.observers.UserlistObserver;
+import de.haw.rnp.client.view.ViewController;
+import de.haw.rnp.util.AddressType;
+import javafx.collections.ObservableList;
+
+import javax.swing.text.View;
+import java.util.ArrayList;
+
 public class Controller implements IControllerService {
 
-    private MainApp mainApp;
+    private ObservableList<User> users;
+    private ViewController viewController;
+    private MessageObserver messageObserver;
+    private UserlistObserver userlistObserver;
+    private IIncomingClientAdapterServices adapterServices;
 
-    public Controller(){}
-
-    public void setMainApp(MainApp mainApp){
-        this.mainApp = mainApp;
+    public Controller(ObservableList<User> users){
+        this.users = users;
     }
 
+    @Override
+    public boolean startServer(AddressType localAddress) {
+        return false;
+    }
+
+    @Override
+    public boolean sendLogin(String username, AddressType recipient) {
+        return false;
+    }
+
+    @Override
+    public boolean sendMessage(String message, ArrayList<AddressType> recipients) {
+        return false;
+    }
+
+    @Override
+    public void sendLogout() {
+
+    }
 }
