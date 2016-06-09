0d9d0738af8369e51922a693f8d07a51863644a4
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-09 18:20:25 +0200

    repo now stores current local server address and username

--- exercise02/TransportComponent/src/de/haw/rnp/component/transport/dataaccesslayer/TransportRepository.java
+++ exercise02/TransportComponent/src/de/haw/rnp/component/transport/dataaccesslayer/TransportRepository.java
@@ -1,4 +1,22 @@
 package de.haw.rnp.component.transport.dataaccesslayer;
 
+import de.haw.rnp.adapter.outgoingclient.dataaccesslayer.UserDTO;
+import de.haw.rnp.component.transport.dataaccesslayer.entities.Frame;
+import de.haw.rnp.util.AddressType;
+
 public class TransportRepository {
+
+    private UserDTO local;
+
+    public TransportRepository(Frame frame){
+        local = frame.toUserDTO();
+    }
+
+    public TransportRepository(AddressType address){
+        local = new UserDTO(address, "");
+    }
+
+    public UserDTO getLocal(){
+        return local;
+    }
 }
