6937b0ba4ca4b86c82ebabc922614bab7aedcb0d
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-09 08:52:12 +0200

    implemented message observer, might change

--- exercise02/Client/src/de/haw/rnp/client/observers/MessageObserver.java
+++ exercise02/Client/src/de/haw/rnp/client/observers/MessageObserver.java
@@ -1,6 +1,8 @@
 package de.haw.rnp.client.observers;
 
 import de.haw.rnp.adapter.outgoingclient.dataaccesslayer.MessageDTO;
+import de.haw.rnp.client.Controller;
+import de.haw.rnp.client.view.ViewController;
 import de.haw.rnp.util.ChatUtil;
 import de.haw.rnp.util.IObserver;
 
@@ -8,8 +10,14 @@ import java.util.ArrayList;
 
 public class MessageObserver implements IObserver<MessageDTO>{
 
+    private ViewController controller;
+
+    public void setController(ViewController controller){
+        this.controller = controller;
+    }
+
     @Override
     public void update(MessageDTO elem) {
-
+        controller.appendMessage(elem.getSender(), elem.getMessage());
     }
 }
