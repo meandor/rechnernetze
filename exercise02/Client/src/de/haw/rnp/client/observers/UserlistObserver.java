48b6f2bbb89fbba87ea1ffefe83e9b505229ddc5
Sebastian Wientzek <sebastian.wientzek@haw-hamburg.de>
2016-06-09 09:36:50 +0200

    implemented UserlistObserver

--- exercise02/Client/src/de/haw/rnp/client/observers/UserlistObserver.java
+++ exercise02/Client/src/de/haw/rnp/client/observers/UserlistObserver.java
@@ -1,13 +1,40 @@
 package de.haw.rnp.client.observers;
 
 import de.haw.rnp.adapter.outgoingclient.dataaccesslayer.UserDTO;
+import de.haw.rnp.client.model.User;
+import de.haw.rnp.util.AddressType;
 import de.haw.rnp.util.IObserver;
+import javafx.collections.ObservableList;
 
 import java.util.ArrayList;
 
 public class UserlistObserver implements IObserver<ArrayList<UserDTO>> {
+
+    private ObservableList<User> users;
+
+    public UserlistObserver(ObservableList<User> users){
+        this.users = users;
+    }
+
     @Override
     public void update(ArrayList<UserDTO> elem) {
+        for(UserDTO e : elem){
+            User tmp = findUserByAddress(e.getAddress());
+            if(tmp != null){
+                int i = users.indexOf(tmp);
+                tmp.setName(e.getName());
+                users.set(i, tmp);
+            }else{
+                users.add(new User(e.getName(), e.getAddress()));
+            }
+        }
+    }
 
+    private User findUserByAddress(AddressType address){
+        for(User user : users){
+            if(user.getAddress().equals(address))
+                return user;
+        }
+        return null;
     }
 }
