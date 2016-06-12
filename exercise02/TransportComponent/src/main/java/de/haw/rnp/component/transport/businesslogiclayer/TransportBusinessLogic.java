1a43a8ee5dd3cff1d6001f5a09c01055195b7ec8
Daniel Schruhl <danielschruhl@gmail.com>
2016-06-12 13:06:20 +0200

    sneding propagate to everybody except myself

--- exercise02/TransportComponent/src/main/java/de/haw/rnp/component/transport/businesslogiclayer/TransportBusinessLogic.java
+++ exercise02/TransportComponent/src/main/java/de/haw/rnp/component/transport/businesslogiclayer/TransportBusinessLogic.java
@@ -7,9 +7,9 @@ import de.haw.rnp.component.transport.accesslayer.ITransportServicesForIncomingP
 import de.haw.rnp.component.transport.dataaccesslayer.TransportRepository;
 import de.haw.rnp.component.transport.dataaccesslayer.entities.Field;
 import de.haw.rnp.component.transport.dataaccesslayer.entities.Frame;
-import de.haw.rnp.util.enumerations.FieldType;
 import de.haw.rnp.util.AddressType;
 import de.haw.rnp.util.ChatUtil;
+import de.haw.rnp.util.enumerations.FieldType;
 import de.haw.rnp.util.enumerations.MessageType;
 
 import java.net.InetAddress;
@@ -24,7 +24,7 @@ public class TransportBusinessLogic implements ITransportServices, ITransportSer
     private IOutgoingPeerAdapterServices outgoingPeerAdapterServices;
     private TransportRepository repo;
 
-    public TransportBusinessLogic( IOutgoingPeerAdapterServices outgoingPeerAdapterServices){
+    public TransportBusinessLogic(IOutgoingPeerAdapterServices outgoingPeerAdapterServices) {
         this.outgoingPeerAdapterServices = outgoingPeerAdapterServices;
     }
 
@@ -62,19 +62,18 @@ public class TransportBusinessLogic implements ITransportServices, ITransportSer
 
     @Override
     public void propagatePeer(Frame frame, Collection<UserDTO> recipients) {
-        System.out.println("propagate: " + frame.toUserDTO().getName());
-        AddressType sender = frame.getSender();
-        for(UserDTO recipient : recipients){
-            if(!recipient.getAddress().equals(sender) && !checkLocalHelper(frame, recipient)){
+        System.out.println("propagate: " + frame.toUserDTO().getAddress().getIp().getHostAddress());
+        for (UserDTO recipient : recipients) {
+            if (!checkLocalHelper(frame, recipient)) {
                 frame.setSender(repo.getLocal().getAddress());
                 frame.setRecipient(recipient.getAddress());
                 sendLogin(frame);
             }
         }
-        if(!repo.getLocal().getName().equals("")) {
-            Frame myname = new Frame(repo.getLocal().getAddress(), frame.toUserDTO().getAddress(), 1, MessageType.MyName, 1);
-            myname.addField(new Field<>(FieldType.Name, repo.getLocal().getName().length(), repo.getLocal().getName()));
-            sendUsername(myname);
+        if (!repo.getLocal().getName().equals("")) {
+            Frame myName = new Frame(repo.getLocal().getAddress(), frame.toUserDTO().getAddress(), 1, MessageType.MyName, 1);
+            myName.addField(new Field<>(FieldType.Name, repo.getLocal().getName().length(), repo.getLocal().getName()));
+            sendUsername(myName);
         }
     }
 
@@ -82,8 +81,8 @@ public class TransportBusinessLogic implements ITransportServices, ITransportSer
     public void propagateLogout(Frame frame, Collection<UserDTO> recipients) {
         AddressType sender = frame.getSender();
         frame.setSender(repo.getLocal().getAddress());
-        for(UserDTO recipient : recipients){
-            if(!recipient.getAddress().equals(sender)) {
+        for (UserDTO recipient : recipients) {
+            if (!recipient.getAddress().equals(sender)) {
                 frame.setRecipient(recipient.getAddress());
                 sendLogout(frame);
             }
@@ -95,23 +94,33 @@ public class TransportBusinessLogic implements ITransportServices, ITransportSer
         return checkLocalHelper(frame, repo.getLocal());
     }
 
-    private boolean checkLocalHelper(Frame frame, UserDTO user){
+    private boolean checkLocalHelper(Frame frame, UserDTO user) {
         return frame.toUserDTO().getAddress().equals(user.getAddress());
     }
 
-    private Collection<Field> parseFields(byte[] bytes){
+    private Collection<Field> parseFields(byte[] bytes) {
         Collection<Field> fields = new ArrayList<>();
 
-        while(bytes.length > 0){
+        while (bytes.length > 0) {
             FieldType fieldType = FieldType.fromBytes(Arrays.copyOf(bytes, 2));
             int length = ChatUtil.byteArrayToInt(Arrays.copyOfRange(bytes, 2, 4));
             bytes = ChatUtil.cut(bytes, 4);
-            switch(fieldType){
-                case IP: fields.add(parseIP(Arrays.copyOf(bytes, length), fieldType, length)); break;
-                case Port: fields.add(parsePort(Arrays.copyOf(bytes, length), fieldType, length)); break;
-                case Name: fields.add(parseName(Arrays.copyOf(bytes, length), fieldType, length)); break;
-                case Text: fields.add(parseText(Arrays.copyOf(bytes, length), fieldType, length)); break;
-                case UserList: fields.add(parseUserlist(Arrays.copyOf(bytes, length), fieldType, length)); break;
+            switch (fieldType) {
+                case IP:
+                    fields.add(parseIP(Arrays.copyOf(bytes, length), fieldType, length));
+                    break;
+                case Port:
+                    fields.add(parsePort(Arrays.copyOf(bytes, length), fieldType, length));
+                    break;
+                case Name:
+                    fields.add(parseName(Arrays.copyOf(bytes, length), fieldType, length));
+                    break;
+                case Text:
+                    fields.add(parseText(Arrays.copyOf(bytes, length), fieldType, length));
+                    break;
+                case UserList:
+                    fields.add(parseUserlist(Arrays.copyOf(bytes, length), fieldType, length));
+                    break;
             }
             bytes = ChatUtil.cut(bytes, length);
         }
@@ -119,7 +128,7 @@ public class TransportBusinessLogic implements ITransportServices, ITransportSer
         return fields;
     }
 
-    private Field parseIP(byte[] bytes, FieldType fieldType, int length){
+    private Field parseIP(byte[] bytes, FieldType fieldType, int length) {
         try {
             InetAddress address = InetAddress.getByAddress(bytes);
             return new Field<>(fieldType, length, address);
@@ -129,21 +138,21 @@ public class TransportBusinessLogic implements ITransportServices, ITransportSer
         }
     }
 
-    private Field parsePort(byte[] bytes, FieldType fieldType, int length){
+    private Field parsePort(byte[] bytes, FieldType fieldType, int length) {
         return new Field<>(fieldType, length, ChatUtil.byteArrayToInt(bytes));
     }
 
-    private Field parseName(byte[] bytes, FieldType fieldType, int length){
-        return  new Field<>(fieldType, length, new String(bytes, StandardCharsets.US_ASCII));
-    }
-
-    private Field parseText(byte[] bytes, FieldType fieldType, int length){
+    private Field parseName(byte[] bytes, FieldType fieldType, int length) {
         return new Field<>(fieldType, length, new String(bytes, StandardCharsets.US_ASCII));
     }
 
-    private Field parseUserlist(byte[] bytes, FieldType fieldType, int length){
+    private Field parseText(byte[] bytes, FieldType fieldType, int length) {
+        return new Field<>(fieldType, length, new String(bytes, StandardCharsets.US_ASCII));
+    }
+
+    private Field parseUserlist(byte[] bytes, FieldType fieldType, int length) {
         Collection<AddressType> addresses = new ArrayList<>();
-        for(int x = 0; x < (length / 8); x++){
+        for (int x = 0; x < (length / 8); x++) {
             try {
 
                 InetAddress ip = InetAddress.getByAddress(Arrays.copyOf(bytes, 4));
