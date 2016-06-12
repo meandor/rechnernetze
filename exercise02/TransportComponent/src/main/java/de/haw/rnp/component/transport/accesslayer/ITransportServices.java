3a00e29feb75e2507aec92893b450ee93b76397b
Daniel Schruhl <danielschruhl@gmail.com>
2016-06-12 12:51:06 +0200

    more javadoc

--- exercise02/TransportComponent/src/main/java/de/haw/rnp/component/transport/accesslayer/ITransportServices.java
+++ exercise02/TransportComponent/src/main/java/de/haw/rnp/component/transport/accesslayer/ITransportServices.java
@@ -9,17 +9,34 @@ import de.haw.rnp.util.AddressType;
 public interface ITransportServices {
 
     /**
-     * Sends a Text Message
-     * @param frame
+     * Sends a Text Message.
+     * @param frame Frame containing more info
      */
     void sendMessage(Frame frame);
 
+    /**
+     * Sends a Login Message.
+     * @param frame Frame containing required info
+     */
     void sendLogin(Frame frame);
 
+    /**
+     * Sends a Logout Message.
+     * @param frame Frame containing required info
+     */
     void sendLogout(Frame frame);
 
+    /**
+     * Sends a Username Message.
+     * @param frame Frame containing required info
+     */
     void sendUsername(Frame frame);
 
+    /**
+     * TODO: Sebastian
+     * @param address
+     * @param name
+     */
     void setLocal(AddressType address, String name);
 
 }
--- exercise02/TransportComponent/src/main/java/de/haw/rnp/component/transport/accesslayer/ITransportServicesForIncomingPeerAdapter.java
+++ exercise02/TransportComponent/src/main/java/de/haw/rnp/component/transport/accesslayer/ITransportServicesForIncomingPeerAdapter.java
@@ -5,14 +5,42 @@ import de.haw.rnp.component.transport.dataaccesslayer.entities.Frame;
 
 import java.util.Collection;
 
+/**
+ * Interface for the IncomingPeers and the TransportServices.
+ * <p>
+ * Defines the available functions provided by the TransportService for the Incoming Peers.
+ */
 public interface ITransportServicesForIncomingPeerAdapter {
 
+    /**
+     * Receives a Frame as a byte stream.
+     *
+     * @param bytes byte[] incoming byte stream
+     * @return Frame representation of the bytes
+     */
     Frame receiveFrameAsBytes(byte[] bytes);
 
+    /**
+     * Propagate the Frame to all the recipients
+     *
+     * @param frame      Frame with info
+     * @param recipients Collection<UserDTO> with all recipients
+     */
     void propagatePeer(Frame frame, Collection<UserDTO> recipients);
 
+    /**
+     * Propagate a Logout to all the recipients
+     *
+     * @param frame      Frame with info
+     * @param recipients Collection<UserDTO> with all recipients
+     */
     void propagateLogout(Frame frame, Collection<UserDTO> recipients);
 
+    /**
+     * TODO: Sebastian
+     *
+     * @param frame
+     * @return
+     */
     boolean checkLocal(Frame frame);
-
 }
