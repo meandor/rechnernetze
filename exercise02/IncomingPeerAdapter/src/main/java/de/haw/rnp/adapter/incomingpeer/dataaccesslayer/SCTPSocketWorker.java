98c30bdcc0e9db3fa00e66dbe653684a747de6d3
Daniel Schruhl <danielschruhl@gmail.com>
2016-06-22 14:06:06 +0200

    waiting for 10 seconds for the worker now

--- exercise02/IncomingPeerAdapter/src/main/java/de/haw/rnp/adapter/incomingpeer/dataaccesslayer/SCTPSocketWorker.java
+++ exercise02/IncomingPeerAdapter/src/main/java/de/haw/rnp/adapter/incomingpeer/dataaccesslayer/SCTPSocketWorker.java
@@ -5,6 +5,8 @@ import com.sun.nio.sctp.SctpChannel;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.concurrent.BlockingQueue;
 
 /**
@@ -16,7 +18,7 @@ public class SCTPSocketWorker extends SocketWorker {
 
     private SctpChannel clientSocket;
     private MessageInfo messageInfo;
-    private byte[] readBytes;
+    private Instant readingTime;
 
     public SCTPSocketWorker(SctpChannel clientSocket, BlockingQueue<byte[]> queue) {
         super(queue);
@@ -25,26 +27,31 @@ public class SCTPSocketWorker extends SocketWorker {
 
     @Override
     public void run() {
-        ByteBuffer buf = ByteBuffer.allocateDirect(512);
-        messageInfo = null;
-        if (clientSocket.isBlocking()) {
+        readingTime = Instant.now();
+        System.out.println("SCTP Worker started!");
+        while (Duration.between(readingTime, Instant.now()).getSeconds() < 10) {
+            ByteBuffer buf = ByteBuffer.allocateDirect(512);
+            messageInfo = null;
+            if (clientSocket.isBlocking()) {
+                try {
+                    clientSocket.configureBlocking(false);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
             try {
-                clientSocket.configureBlocking(false);
+                messageInfo = clientSocket.receive(buf, null, null);
+                if (messageInfo != null && messageInfo.bytes() > 0) {
+                    readingTime = Instant.now();
+                    byte[] data = new byte[messageInfo.bytes()];
+                    buf.position(0);
+                    buf.get(data);
+                    queue.offer(data);
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-
-        try {
-            messageInfo = clientSocket.receive(buf, null, null);
-            if (messageInfo != null && messageInfo.bytes() > 0) {
-                byte[] data = new byte[messageInfo.bytes()];
-                buf.position(0);
-                buf.get(data);
-                queue.offer(data);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
